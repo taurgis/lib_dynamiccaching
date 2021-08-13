68d110f92f1bd3e59f6cf2cf0bdd8ce6569684db
Thomas Theunen <thomas.theunen@forward.eu>
2021-08-13 22:22:38 +0200

    Add scenario testing

--- test/unit/lib_dynamiccaching/scripts/helpers/cacheHelpers.js
+++ test/unit/lib_dynamiccaching/scripts/helpers/cacheHelpers.js
@@ -40,7 +40,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(productStub.availabilityModel.timeToOutOfStock);
     });
 
-    it('should round down if a decimal value is returned by Salesforce.', () => {
+    it('should round down if a decimal value is returned.', () => {
         productStub.availabilityModel.timeToOutOfStock = 10.6;
 
         const result = cacheHelpers.calculateProductCacheTime(productStub);
@@ -56,14 +56,6 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(cacheHelpers.FALLBACK_CACHE_TIME);
     });
 
-    it('should return the fallback value if the "Time To Out Of Stock" is unknown (not able to calculate).', () => {
-        productStub.availabilityModel.timeToOutOfStock = 0;
-
-        const result = cacheHelpers.calculateProductCacheTime(productStub);
-
-        expect(result).to.equal(cacheHelpers.FALLBACK_CACHE_TIME);
-    });
-
     it('should return the fallback value if the product is no longer available to order.', () => {
         productStub.availabilityModel.isOrderable = () => false;
 
@@ -72,7 +64,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(cacheHelpers.FALLBACK_CACHE_TIME);
     });
 
-    it('should return the information from the master if the product is a variant.', () => {
+    it('should return the information from the master if the product is a "Variant".', () => {
         productStub.isVariant = () => true;
         productStub.variationModel = {
             master: {
@@ -89,7 +81,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(productStub.variationModel.master.availabilityModel.timeToOutOfStock);
     });
 
-    it('should return the information from the master if the product is a variation group.', () => {
+    it('should return the information from the master if the product is a "Variation Group".', () => {
         productStub.isVariationGroup = () => true;
         productStub.variationModel = {
             master: {
@@ -106,7 +98,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(productStub.variationModel.master.availabilityModel.timeToOutOfStock);
     });
 
-    it('should return a value for a standard product when active data is available.', () => {
+    it('should return a value for a standard product when Active Data is available for the past month.', () => {
         productStub.activeData = {
             salesVelocityWeek: 1,
             salesVelocityMonth: 1
@@ -117,7 +109,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(15);
     });
 
-    it('should return the maximum value if the calculated hours is higher than that with Active Data taken into account for week and month..', () => {
+    it('should return the maximum value if the calculated value is higher.', () => {
         productStub.activeData = {
             salesVelocityWeek: 0.5,
             salesVelocityMonth: 0.3
@@ -128,7 +120,7 @@ describe('Dynamic Caching', () => {
         expect(result).to.equal(cacheHelpers.LONG_CACHE_TIME);
     });
 
-    it('should return the minimum value if the calculated hours is lower than that with Active Data taken into account for week and month..', () => {
+    it('should return the minimum value if the calculated hours is lower the minimum.', () => {
         productStub.activeData = {
             salesVelocityWeek: 30,
             salesVelocityMonth: 30
@@ -152,5 +144,138 @@ describe('Dynamic Caching', () => {
         const result = cacheHelpers.calculateProductCacheTime(productStub);
 
         expect(result).to.equal(productStub.availabilityModel.timeToOutOfStock);
+    });
+
+    it('Scenario: Low Sales with high stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 0.1,
+            salesVelocityMonth: 0.1
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 2000,
+            inventoryRecord: {
+                ATS: 200
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(cacheHelpers.LONG_CACHE_TIME);
+    });
+
+    it('Scenario: High Sales with high stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 50, // 50 results in 4 hours
+            salesVelocityMonth: 50
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 8,
+            inventoryRecord: {
+                ATS: 200
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(6);
+    });
+
+    it('Scenario: High Sales with low stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 50, // 50 results in 0.2 hours
+            salesVelocityMonth: 50
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 1,
+            inventoryRecord: {
+                ATS: 10
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(1);
+    });
+
+    it('Scenario: Low Sales with low stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 0.2,
+            salesVelocityMonth: 0.2
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 50,
+            inventoryRecord: {
+                ATS: 4
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(22);
+    });
+
+    it('Scenario: Medium Sales with low stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 0.8,
+            salesVelocityMonth: 0.7
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 6,
+            inventoryRecord: {
+                ATS: 4
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(5);
+    });
+
+    it('Scenario: Medium Sales with medium stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 0.8,
+            salesVelocityMonth: 0.7
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 42,
+            inventoryRecord: {
+                ATS: 30
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(24);
+    });
+
+    it('Scenario: Medium Sales with high stock.', () => {
+        productStub.activeData = {
+            salesVelocityWeek: 0.8,
+            salesVelocityMonth: 0.7
+        };
+
+        productStub.availabilityModel = {
+            isOrderable: () => true,
+            timeToOutOfStock: 42,
+            inventoryRecord: {
+                ATS: 200
+            }
+        };
+
+        const result = cacheHelpers.calculateProductCacheTime(productStub);
+
+        expect(result).to.equal(24);
     });
 });
